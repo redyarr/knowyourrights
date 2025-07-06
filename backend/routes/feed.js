366c6380f0d01f1096a9b5b22d99f015bcda40e3
r3k4r <rekarjamal33@gmail.com>
2025-07-06 10:14:54 +0300

    Update feed.js

--- backend/routes/feed.js
+++ backend/routes/feed.js
@@ -25,7 +25,7 @@ const upload = multer({
 // Create post route
 
 // Feed routes
-router.get('/',isAuthenticated, FeedController.getAllPosts);
+router.get('/', FeedController.getAllPosts);
 
 // Post reaction and comment routes
 router.post('/create-post', isAuthenticated, isVerifiedLawyer, upload.single('image'), FeedController.createPost);
